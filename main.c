379ac60ce7c6505253751797c372a9fb3c06f83b
Gonzo <carloseaf@gmail.com>
2018-07-04 14:16:07 -0300

    Remove line breaks

--- main.c
+++ main.c
@@ -5,23 +5,19 @@
 
 #define PI ( 3.1415926535 )
 
-char
-is_even(int value) {  // Retorna se o número é par
+char is_even(int value) {  // Retorna se o número é par
     return value % 2 == 0;
 }
 
-char
-is_odd(int value) { // Retorna se o número é ímpar
+char is_odd(int value) { // Retorna se o número é ímpar
     return !is_even(value);
 }
 
-double
-f(double t) {  // função f(x)
+double f(double t) {  // função f(x)
     return 1.0/sqrt(2 * PI) * exp(-(t*t)/2);
 }
 
-double
-int_f(double from, double to, int points) {  // ∫f(x)  Pelo método de Simpson
+double int_f(double from, double to, int points) {  // ∫f(x)  Pelo método de Simpson
     assert(is_odd(points));
     double h = (to - from)/(points - 1);
     double acc = 0;
@@ -35,13 +31,11 @@ int_f(double from, double to, int points) {  // ∫f(x)  Pelo método de Simpson
     return h/3.0 * acc;
 }
 
-double
-F(double x) {  // equação proposta
+double F(double x) {  // equação proposta
     return int_f(0.0, x, 13) - 0.45;
 }
 
-double
-newton_root(  // método de newton
+double newton_root(  // método de newton
         double (*f)(double), // função para achar a raiz
         double (*f_prime)(double), // funcão derivada
         double x0, // ponto inicial
@@ -58,8 +52,7 @@ newton_root(  // método de newton
     return x;
 }
 
-int
-main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 
     printf("Sendo F(1) = %lf\n", F(1));
     printf("Sendo F(2) = %lf\n", F(2));
